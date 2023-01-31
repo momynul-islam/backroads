f14bb695d0f7581ccfb8bb0f77d737c0db590a2b
momynul-islam <islammomynul.cod@gmail.com>
2023-01-31 08:39:35 +0600

    nav and header section implemented

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const menuBar = document.querySelector(".menu-bar");
+const links = document.querySelector(".links");
+
+menuBar.addEventListener("click", () => {
+  links.classList.toggle("show");
+});
